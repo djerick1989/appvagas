08f9f065779aa77345df31194dfbc77edef94753
fabian.urrutia <fabian.urrutia@simplit-solutions.com>
2020-11-09 23:11:04 -0300

    Added a lot of improvements for working better

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import SplashScreen from './Screen/SplashScreen';
@@ -14,15 +15,16 @@ import ChangePassScreen from './Screen/ChangePassScreen';
 import ConfirmCodeScreen from './Screen/ConfirmCodeScreen';
 import RegisterScreen from './Screen/RegisterScreen';
 import HomeScreen from './Screen/NavScreen/HomeScreen';
-import AsyncStorage from '@react-native-community/async-storage'
-
-
-
+import AsyncStorage from '@react-native-community/async-storage';
 
 const HomeStack = createStackNavigator();
 const HomeStackScreen = () => (
   <HomeStack.Navigator>
-    <HomeStack.Screen name="Home" component={HomeScreen}  options={{headerShown: false}}/>
+    <HomeStack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{headerShown: false}}
+    />
   </HomeStack.Navigator>
 );
 
@@ -48,14 +50,10 @@ const AppTabsScreen = () => (
       component={HomeStackScreen}
       options={{
         tabBarLabel: 'Home',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons
-            name="home"
-            color={color}
-            size={size}
-          />
+        tabBarIcon: ({color, size}) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
         ),
-      }} 
+      }}
     />
     {/* <AppTabs.Screen
       name="List"
@@ -86,62 +84,97 @@ const AppTabsScreen = () => (
   </AppTabs.Navigator>
 );
 
-
 const AuthStack = createStackNavigator();
 const AuthStackScreen = () => (
   <AuthStack.Navigator>
-    <AuthStack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false,}} />
-    <AuthStack.Screen name="SlideScreen" component={SlideScreen} options={{headerShown: false,}}/>
-    <AuthStack.Screen name="StartScreen" component={StartScreen} options={{headerShown: false,}}/>
-    <AuthStack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false,}}/>
-    <AuthStack.Screen name="ForgetPassScreen" component={ForgetPassScreen} options={{headerShown: false,}}/>
-    <AuthStack.Screen name="ChangePassScreen" component={ChangePassScreen} options={{headerShown: false,}}/>
-    <AuthStack.Screen name="ConfirmCodeScreen" component={ConfirmCodeScreen} options={{headerShown: false,}}/>
-    <AuthStack.Screen name="RegisterScreen" component={RegisterScreen} options={{headerShown: false,}}/>
+    <AuthStack.Screen
+      name="SplashScreen"
+      component={SplashScreen}
+      options={{headerShown: false}}
+    />
+    <AuthStack.Screen
+      name="SlideScreen"
+      component={SlideScreen}
+      options={{headerShown: false}}
+    />
+    <AuthStack.Screen
+      name="StartScreen"
+      component={StartScreen}
+      options={{headerShown: false}}
+    />
+    <AuthStack.Screen
+      name="LoginScreen"
+      component={LoginScreen}
+      options={{headerShown: false}}
+    />
+    <AuthStack.Screen
+      name="ForgetPassScreen"
+      component={ForgetPassScreen}
+      options={{headerShown: false}}
+    />
+    <AuthStack.Screen
+      name="ChangePassScreen"
+      component={ChangePassScreen}
+      options={{headerShown: false}}
+    />
+    <AuthStack.Screen
+      name="ConfirmCodeScreen"
+      component={ConfirmCodeScreen}
+      options={{headerShown: false}}
+    />
+    <AuthStack.Screen
+      name="RegisterScreen"
+      component={RegisterScreen}
+      options={{headerShown: false}}
+    />
   </AuthStack.Navigator>
 );
 
 export default () => {
-    const [userToken , setToken] = React.useState('');
-    const deleteData = async () => {
-        try {
-          await AsyncStorage.removeItem('token');
-        } catch (error) {
-          // Error retrieving data
-          console.log(error.message);
-        }
-      }
-    const readData = async () => {
-        await AsyncStorage.getItem('token').then((res) => {
-            // if (userToken !== null) {
-            //     setToken(res)
-            //   }
-            console.log(res);
-            setToken(res);
-          });
-         
-        // try {
-            
-        // } catch (e) {
-        //   alert('Failed to fetch the data from storage')
-        // }
-      }
+  const [userToken, setToken] = React.useState('');
+  const deleteData = async () => {
+    try {
+      await AsyncStorage.removeItem('token');
+    } catch (error) {
+      // Error retrieving data
+      console.log(error.message);
+    }
+  };
+  const readData = async () => {
+    await AsyncStorage.getItem('token').then((res) => {
+      // if (userToken !== null) {
+      //     setToken(res)
+      //   }
+      console.log(res);
+      setToken(res);
+    });
+
+    // try {
+
+    // } catch (e) {
+    //   alert('Failed to fetch the data from storage')
+    // }
+  };
   React.useEffect(() => {
     setTimeout(() => {
-    //   setIsLoading(!isLoading);
-    // if (userToken == '')
-    // readData()
-    deleteData()
+      //   setIsLoading(!isLoading);
+      // if (userToken == '')
+      // readData()
+      deleteData();
     }, 500);
   }, []);
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
+  
   return (
-    <NavigationContainer>
-        {userToken == ''?(
-        <AuthStackScreen />
-        ):(
-        <AppTabsScreen/> 
-        )}
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        {userToken == '' ? <AuthStackScreen /> : <AppTabsScreen />}
+      </NavigationContainer>
+    </SafeAreaView>
   );
 };
--- Screen/StartScreen.js
+++ Screen/StartScreen.js
@@ -1,67 +1,65 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Image,TouchableOpacity, Dimensions} from "react-native";
- 
-const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+
+const {width: viewportWidth, height: viewportHeight} = Dimensions.get('window');
 export default class StartScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      
-    };
+    this.state = {};
   }
- 
+
   render() {
     return (
-        <View style={styles.container}>
-            
-                <View style={{ alignItems: 'center',flex:1 }}>
-                    <Image
-                    source={require('../Image/Logo-Pesquisa-Vagas.png')}
-                    style={{
-                        width: '70%',
-                        height: 100,
-                        resizeMode: 'contain',
-                        margin: 20,
-                        top:0,
-                        
-                      }}
-                    />
-                </View>
-                <View style={styles.SectionStyle}>
-                    <Image 
-                    source={require('../Image/6.png')}
-                    style={{
-                        width: '100%',
-                        height:viewportHeight*0.55,
-                        resizeMode: 'contain',
-                        top:0,
-                        
-                      }}/>
-                </View>
-                <View style={{alignItems:'center',}}>
-                    
-                  <TouchableOpacity
-                  style={styles.buttonStyle}
-                  activeOpacity={0.5}
-                  onPress={() =>this.props.navigation.navigate('RegisterScreen')}
-                  >
-                  <Text style={styles.buttonTextStyle}>Cadastrar</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                  style={styles.loginbuttonStyle}
-                  activeOpacity={0.5}
-                  onPress={() =>this.props.navigation.navigate('LoginScreen')}
-                  >
-                  <Text style={styles.loginbuttonTextStyle}>Login</Text>
-                  </TouchableOpacity>
-                
-                </View>
-            
+      <View style={styles.container}>
+        <View style={{alignItems: 'center', flex: 1}}>
+          <Image
+            source={require('../Image/Logo-Pesquisa-Vagas.png')}
+            style={{
+              width: '70%',
+              height: 100,
+              resizeMode: 'contain',
+              margin: 20,
+              top: 0,
+            }}
+          />
         </View>
+        <View style={styles.SectionStyle}>
+          <Image
+            source={require('../Image/6.png')}
+            style={{
+              width: '100%',
+              height: viewportHeight * 0.55,
+              resizeMode: 'contain',
+              top: 0,
+            }}
+          />
+        </View>
+        <View style={{alignItems: 'center'}}>
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            activeOpacity={0.5}
+            onPress={() => this.props.navigation.navigate('RegisterScreen')}>
+            <Text style={styles.buttonTextStyle}>Cadastrar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.loginbuttonStyle}
+            activeOpacity={0.5}
+            onPress={() => this.props.navigation.navigate('LoginScreen')}>
+            <Text style={styles.loginbuttonTextStyle}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -69,7 +67,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
   },
   SectionStyle: {
-    flex:4,
+    flex: 4,
     marginTop: 0,
   },
   buttonStyle: {
@@ -78,7 +76,7 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     borderColor: '#6948F4',
     height: 40,
-    width:viewportWidth*0.45,
+    width: viewportWidth * 0.45,
     alignItems: 'center',
     borderRadius: 30,
     marginLeft: 35,
@@ -92,7 +90,7 @@ const styles = StyleSheet.create({
     color: '#6948F4',
     borderColor: '#6948F4',
     height: 40,
-    width:viewportWidth*0.45,
+    width: viewportWidth * 0.45,
     alignItems: 'center',
     borderRadius: 30,
     marginLeft: 35,
@@ -102,7 +100,7 @@ const styles = StyleSheet.create({
   },
   buttonTextStyle: {
     color: '#FFFFFF',
-    
+
     paddingVertical: 8,
     fontSize: 18,
   },
@@ -111,5 +109,4 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     fontSize: 18,
   },
-  
 });
